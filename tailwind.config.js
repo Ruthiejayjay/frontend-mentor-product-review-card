a688fd7659e65b3a56cfd88e28df153faf5560c0
Ruth James <jamesruth449@gmail.com>
2023-05-02 15:06:26 +0100

    setup completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,17 @@ module.exports = {
   content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    colors: {
+      'dark-cyan': 'hsl(158, 36%, 37%)',
+      'cream': 'hsl(30, 38%, 92%)',
+      'very-dark-blue': 'hsl(212, 21%, 14%)',
+      'dark-grayish-blue': 'hsl(228, 12%, 48%)',
+      'white': 'hsl(0, 0%, 100%)'
+    },
+    fontFamily: {
+      Fraunces: ['Fraunces', 'cursive'],
+      Montserrat: ['Montserrat', 'cursive']
+    },
   },
   plugins: [],
 }
